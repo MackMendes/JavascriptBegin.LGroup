15be7ba953372fa9363600c3a0c7e848d12e07d0
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2015-02-22 23:28:02 -0300

    Palestra JS - 22-02-2015
    Palestra de JavaScript na LGroup no dia 22/02/2015.

---
+++ Feito na Palestra - 22-02-2015/Scripts/cadastro.js
@@ -0,0 +1,193 @@
+﻿// [] = Estamos criando uma variavel global, do tipo Array.
+// {} = Criação de Objeto
+var ListCidades = [
+ { texto: "São Paulo", valor: "SP" },
+ { texto: "Rio de Janeiro", valor: "RJ" },
+ { texto: "Minas Gerais", valor: "MG" },
+ { texto: "Bahia", valor: "BA" }
+];
+
+// Declarando uma variavel de Array
+var listClientes = [];
+
+function onCadastrarClick() {
+    RemoveClassErrorInput();
+    RemoveMensagemErro();
+    if (Validacao() === true) {
+
+        var txtNome = document.getElementById('txtNome');
+        var txtTelefone = document.getElementById('txtTelefone');
+        var txtEmail = document.getElementById('txtEmail');
+        var ddlCidade = document.getElementById('ddlCidade');
+        var ckbAtivo = document.getElementById('ckbAtivo');
+
+        // Atribuindo um objeto em JavaScript
+        var cliente = {};
+        cliente.Nome = txtNome.value;
+        cliente.Telefone = txtTelefone.value;
+        cliente.Email = txtEmail.value;
+        cliente.Cidade = ddlCidade.value;
+        cliente.IsAtivo = ckbAtivo.checked;
+
+        listClientes.push(cliente);
+
+        LimparCampos();
+        MensagemSucesso();
+        ListarClientes();
+    }
+}
+
+function Validacao() {
+    var isValido = true;
+
+    var txtNome = document.getElementById('txtNome');
+    var txtTelefone = document.getElementById('txtTelefone');
+    var txtEmail = document.getElementById('txtEmail');
+    var ddlCidade = document.getElementById('ddlCidade');
+
+    // == : É a igual de valor, somente. Mesmo sendo 
+    // em tipos diferentes. Não da total certeza no 
+    // resultado.
+
+    // === : É uma igualdade de valores e tipo! É mais 
+    // fiel no resultado.
+
+    if (txtNome.value === "") {
+        isValido = false;
+        txtNome.classList.add('errorInput');
+    }
+
+    if (txtTelefone.value === "") {
+        isValido = false;
+        txtTelefone.classList.add('errorInput');
+    }
+
+    if (txtEmail.value === "") {
+        isValido = false;
+        txtEmail.classList.add('errorInput');
+    }
+
+    if (ddlCidade.value === "") {
+        isValido = false;
+        ddlCidade.classList.add('errorInput');
+    }
+
+    if (!isValido) {
+        MensagemErro();
+    }
+
+    return isValido;
+}
+
+function RemoveClassErrorInput() {
+    var listInputComClass =
+        document.getElementsByClassName('errorInput');
+
+    var index = 0;
+    while (listInputComClass.length > 0) {
+        listInputComClass[index]
+            .classList.remove('errorInput');
+    }
+}
+
+function MensagemErro() {
+    var mensagem = '<strong>Atenção: </strong>'
+    + 'Por favor, preencha os campos indicados abaixo.';
+
+    AtribuiMensagem(mensagem);
+
+    var divAlert = document.getElementById('divAlert');
+    divAlert.classList.add('alert');
+    divAlert.classList.add('alert-danger');
+}
+
+function RemoveMensagemErro(){
+    AtribuiMensagem('');
+
+    var divAlert = document.getElementById('divAlert');
+    divAlert.classList.remove('alert');
+    divAlert.classList.remove('alert-danger');
+}
+
+function AtribuiMensagem(mensagem){
+    var divAlert = document.getElementById('divAlert');
+    divAlert.innerHTML = mensagem;
+}
+
+function PreencheCidade(){
+    var options = '<option value="">-- Selecione --</option>';
+    var ddlCidade = document.getElementById('ddlCidade');
+    for (var i = 0; ListCidades.length > i; i++) {
+        options += '<option value="' + 
+        ListCidades[i].valor + '">' + 
+        ListCidades[i].texto + '</option>';        
+    }
+
+    ddlCidade.innerHTML = options;
+}
+
+function MensagemSucesso(){
+    var mensagem = '<strong>Sucesso: </strong>'
+    + 'Cliente cadastro com sucesso!';
+
+    AtribuiMensagem(mensagem);
+
+    var divAlert = document.getElementById('divAlert');
+    divAlert.classList.add('alert');
+    divAlert.classList.add('alert-success');
+
+    setTimeout(RemoveMensagemSucesso, 5000);
+}
+
+function RemoveMensagemSucesso(){
+    AtribuiMensagem('');
+
+    var divAlert = document.getElementById('divAlert');
+    divAlert.classList.remove('alert');
+    divAlert.classList.remove('alert-success');
+}
+
+function LimparCampos(){
+    document.getElementById('txtNome').value = '';
+    document.getElementById('txtTelefone').value = '';
+    document.getElementById('txtEmail').value = '';
+    document.getElementById('ddlCidade').selectedIndex = 0;
+    document.getElementById('ckbAtivo').checked = false;
+}
+
+function ListarClientes(){
+    var tbodyListaCliente = 
+    document.getElementById('tbodyListaCliente');
+    var bodyTable = '';
+
+    for (var i = 0; listClientes.length > i; i++) {
+
+        bodyTable += '<tr>';
+
+        bodyTable += '<td>'; 
+        bodyTable += (i + 1).toString();
+        bodyTable += '</td>';
+        bodyTable += '<td>'; 
+        bodyTable += listClientes[i].Nome;
+        bodyTable += '</td>';
+        bodyTable += '<td>'; 
+        bodyTable += listClientes[i].Telefone;
+        bodyTable += '</td>';
+        bodyTable += '<td>'; 
+        bodyTable += listClientes[i].Email;
+        bodyTable += '</td>';
+        bodyTable += '<td>'; 
+        bodyTable += listClientes[i].Cidade;
+        bodyTable += '</td>';
+        bodyTable += '<td>'; 
+        bodyTable += listClientes[i].IsAtivo;
+        bodyTable += '</td>';
+
+        bodyTable += '</tr>';
+
+    }
+
+
+
+    tbodyListaCliente.innerHTML = bodyTable;
+}
---
+++ Feito na Palestra - 22-02-2015/Scripts/index.js
@@ -0,0 +1,19 @@
+﻿function SetAno() {
+    // debugger = É uma palavra chave do JavaScript que, quando o navegador
+    // identificar esse palavra, vai parar a execução. Debugger do JS.
+    debugger;
+    // Abaixo estamos pegando o DOM da Label que esta no Footer
+    var lblTime = document.getElementById("lblTimeYear");
+    // Atribuindo um texto para a Label
+    lblTime.textContent = "2015";
+}
+
+function CarregarNoOnLoadDaPagina(){
+	SetAno();
+	PreencheCidade();
+}
+
+
+// window.onload = É um evento da página que será executado 
+// quando todos os elementos HTML's forem renderizados.
+window.onload = CarregarNoOnLoadDaPagina;
